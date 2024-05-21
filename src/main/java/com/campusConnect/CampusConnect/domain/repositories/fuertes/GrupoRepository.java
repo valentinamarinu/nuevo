3a253a9bc479c86b192ba805b0e9140dc42414d3
Val <anav.usuga@gmail.com>
2024-05-21 18:28:37 -0500

    funciona

--- src/main/java/com/campusConnect/CampusConnect/domain/repositories/fuertes/GrupoRepository.java
+++ src/main/java/com/campusConnect/CampusConnect/domain/repositories/fuertes/GrupoRepository.java
@@ -1,15 +1,13 @@
 package com.campusConnect.CampusConnect.domain.repositories.fuertes;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import org.springframework.stereotype.Repository;
 
 import com.campusConnect.CampusConnect.domain.entities.fuertes.Grupo;
-import com.campusConnect.CampusConnect.domain.entities.fuertes.Usuario;
 
 @Repository
 public interface GrupoRepository extends JpaRepository<Grupo, Long> {
-    @Query(value = "SELECT u FROM usuario u WHERE u.nombre = :nombre AND u.apellido = :apellido")
-    public Usuario findByNombreAndApellido(String nombre, String apellido);
+    // @Query(value = "SELECT u FROM usuario u WHERE u.nombre = :nombre AND u.apellido = :apellido")
+    // public Usuario findByNombreAndApellido(String nombre, String apellido);
 }
